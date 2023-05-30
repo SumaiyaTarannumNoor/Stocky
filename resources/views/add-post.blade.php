b7792d3b769ed8b976ea3d4e15123c6c7486a6dd
Sumaiya Tarannum Noor <sumaiyatarannumnoor@gmail.com>
2023-05-30 21:56:24 +0600

    Update add-post.blade.php

--- resources/views/add-post.blade.php
+++ resources/views/add-post.blade.php
@@ -11,7 +11,7 @@
               <div class = "col-md-6 offset-md-3">
                 <div class = "card">
                   <div class= "card-header" style= "background-color:rgba(2,3,10,0.4)">
-                    Add Product <a href = "/posts" class="btn btn-success">Show my Products</a>
+                    আইটেম যোগ করুন <a href = "/posts" class="btn btn-success">আইটেম দেখুন</a>
                   </div>
                   <div class = "card-body" style= "background-color:red">
                     @if(Session::has('post_created'))
@@ -22,16 +22,16 @@
                     <form method = "POST" action = "{{route('post.create')}}">
                       @csrf 
                       <div class = "form-group" style= "background-color:magenta">
-                        <label for = "title">Product Title</label>
-                        <input type = "text" name = "title" class= "form-control" placeholder="Enter Product Title"/>
+                        <label for = "title">আইটেম এর নাম</label>
+                        <input type = "text" name = "title" class= "form-control" placeholder="আইটেম এর নাম দিন"/>
                       </div>
 
                       <div class="form-group" style= "background-color:red">
-                        <label for = "body">Product Description</label>
-                        <textarea name = "body" class= "form-control" rows= "3" tyle= "background-color:pink" placeholder="Enter Product Details"></textarea>
+                        <label for = "body">আইটেম এর বর্ণনা</label>
+                        <textarea name = "body" class= "form-control" rows= "3" tyle= "background-color:pink" placeholder="আইটেম এর বর্ণনা দিন"></textarea>
                       </div>
                       
-                      <button type="submit" class="btn btn-success">Add Product</button> 
+                      <button type="submit" class="btn btn-success">আইটেম যোগ করুন</button> 
                     </form>
                   </div>
                 </div>
